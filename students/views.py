9edc358b017f215d3116508e7d9f4ba08b0bc9b8
Ken <ahmadsoud63@gmail.com>
2025-04-30 11:12:17 +0300

    edit response

--- students/views.py
+++ students/views.py
@@ -2,9 +2,9 @@ from django.http import JsonResponse
 
 def get_students(request):
     students = [
-        {"id": 1, "name": "Alice", "age": 21},
-        {"id": 2, "name": "Bob", "age": 22},
-        {"id": 3, "name": "Charlie", "age": 20},
-        {"id": 4, "name": "Mani" , "age": 19}
+        {"id": 1, "name": "Alice", "age": 21, "Major":"Computer Science"},
+        {"id": 2, "name": "Bob", "age": 22, "Major":"Engineering"},
+        {"id": 3, "name": "Charlie", "age": 20, "Major":"Physics"},
+        {"id": 4, "name": "Mani" , "age": 19, "Major":"Computer Science"}
     ]
     return JsonResponse({"students": students})
